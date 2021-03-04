d6bd68448e25a2e06d05815327bd487f1c7074a8
LeandroNeves80 <le.marquesn@gmail.com>
2021-03-04 14:50:45 -0300

    Imagem Commit do Zé

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <h1>Burger do Zé</h1>
 Burger do Zé<br>
-... Indiscutível como o mundial do parmera
+... Indiscutível como o mundial do parmera<br>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyWN3yB_-YbszYnwW3NY5adC4gUe0EpXqdg&usqp=CAU"
