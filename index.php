112c0e64fb8cb0c29a19d6b9bb439b5176bddaad
LeandroNeves80 <le.marquesn@gmail.com>
2021-03-04 15:16:10 -0300

    Imagem Commit do Zé

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>Burger do Zé</h1>
 Burger do Zé<br>
 ... Indiscutível como o mundial do parmera<br>
-<img src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2020/09/19/whatsapp-image-2020-09-18-at-155959-768x576.jpeg" style="
-    width: 510px;
-">">
+<img src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2020/09/19/whatsapp-image-2020-09-18-at-155959-768x576.jpeg"  style="
+    width: 510px;"
+    >
